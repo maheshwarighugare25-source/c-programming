d2b4ab77ae5f44f8d42cf8a4b4b443758fb37d4a
maheshwarighugare25-source <maheshwarighugare25@gmail.com>
2026-07-13 10:56:35 +0530

    Add files via upload

--- add.cpp
+++ add.cpp
@@ -3,5 +3,11 @@ using namespace std;
 int main()
 {
 int a,b ;
-cout<<"a+b";
+cout<<"enter any two number:";
+cin>>a>>b;
+cout<<"addition"<<a+b<<endl;
+cout<<"subtraction"<<a-b<<endl;
+cout<<"multiplication"<<a*b<<endl;
+cout<<"division"<<a/b<<endl;
+cout<<"mod"<<a%b<<endl;
 }
