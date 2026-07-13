da60f0fa8bf7ab863f94f8563cc76b724c3814ff
maheshwarighugare25-source <maheshwarighugare25@gmail.com>
2026-07-13 10:56:58 +0530

    Add files via upload

---
+++ area.cpp.cc
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int a,b ;
+cout<<"enter length and breadth of rectangle";
+cin>>a>>b;
+cout<<"perimeter of the rectangle is:"<<2*(a+b);
+cout<<"area of the rectangle is:"<<a*b;
+}
