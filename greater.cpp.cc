0a6709ebcc156e36f6bcccc6dcca5d1da122310c
maheshwarighugare25-source <maheshwarighugare25@gmail.com>
2026-07-20 10:50:35 +0530

    Add files via upload

---
+++ greater.cpp.cc
@@ -0,0 +1,16 @@
+#include<isostream>
+using namespace std;
+int main()
+{
+int num1,num2;
+cout<<"enter any numbers:"
+cin>>num1>>num2>>endl;
+
+if(num1>num2)
+{
+cout<<num1<<"is even "<<num2;
+}
+else
+{
+cout<<num2 "is greater than"<<num1;}
+}
