a63884b2b97df579d697e6f74d73da76a50f6328
maheshwarighugare25-source <maheshwarighugare25@gmail.com>
2026-07-20 10:49:59 +0530

    Add files via upload

---
+++ swap.cpp.cc
@@ -0,0 +1,15 @@
+#include<isostream>
+using namespace std;
+int main()
+{
+int a,b,c;
+cout<<"enter the number";
+cin>>a>>b;
+c=a;
+a=b;
+b=c;
+cout<<"after swapping"<<endl;
+cout<<"first number"<<a<<endl;
+cout<<"second number"<<b<<endl;
+}
+
