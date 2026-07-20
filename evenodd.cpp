df954aa53c9920e1dc4bf2291f580cbb54638948
maheshwarighugare25-source <maheshwarighugare25@gmail.com>
2026-07-20 10:49:22 +0530

    Add files via upload

---
+++ evenodd.cpp
@@ -0,0 +1,16 @@
+#include<isostream>
+using namespace std;
+int main()
+{
+int num;
+cout<<"enter any numbers:"
+cin>>num;
+
+if
+(num%2=0){
+cout<<num<<"is even "<<endl;
+}
+else
+{
+cout<<num<<"is odd";
+}
