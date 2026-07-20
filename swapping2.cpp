905a9044bd7c1cf46c3aa3b1be1aae406737c795
maheshwarighugare25-source <maheshwarighugare25@gmail.com>
2026-07-20 10:50:12 +0530

    Add files via upload

---
+++ swapping2.cpp
@@ -0,0 +1,13 @@
+#include<isostream>
+using namespace std;
+int main()
+{
+cout<<"enter two numbers:"
+cin>>a>>b;
+c=a;
+a=b;
+b=c;
+cout<<"after swapping:"<<endl;
+cout<<"first number:"<<endl;
+cout<<"second number:"<<endl;
+}
