64d62f00a68c3898f3ed6ee3e1db1b067a05829b
maheshwarighugare25-source <maheshwarighugare25@gmail.com>
2026-08-03 09:43:38 +0530

    Add files via upload

---
+++ factorial.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(){
+int n,i;
+unsigned long long factorial = 1;
+printf("Enter an integer ");
+scanf("%d",&n);
+if(n < 0)
+   printf("error! factorial of a negative number doesn't exist");
+   else{
+       for (i=1;i<=n;++i)
+           {
+          factorial *= i;
+           }
+        printf("factorial of %d= %llu",n,factorial);
+    }
+return 0;
+}
