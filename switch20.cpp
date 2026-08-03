ca2be7ed91b45f082788a00e16df8bf53b7fe39b
maheshwarighugare25-source <maheshwarighugare25@gmail.com>
2026-08-03 09:46:42 +0530

    Add files via upload

---
+++ switch20.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+using namespace std;
+int main(){
+ int choice;
+ float num1,num2;
+ cout<<"arithmetic operations menu\n";
+ cout<<"1.addition\n";
+ cout<<"2.subtraction\n";
+ cout<<"3.multiplication\n";
+ cout<<"4.division\n";
+ cout<<"enter your choice(1-4):";
+ cin>>choice;
+ cout<<"enter two numbers:";
+ cin>>num1>>num2;
+ switch(choice){
+       case1.
+        cout<<"result="<<num1+num2;
+        break;
+       case2.
+        cout<<"result="<<num1-num2;
+        break;
+       case3.
+        cout<<"result="<<num1*num2;
+        break;
+       case4.
+        if(num2!=0)
+         cout<<result="<<num1/num2;
+        else
+         cout<<"division by zero is not possible.";
+         break;
+default:
+cout<<"invalid choice."
+}
+return 0;
+}
