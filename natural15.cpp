d53a8b877d23d09b17ce751b6ed256a7ed01259d
maheshwarighugare25-source <maheshwarighugare25@gmail.com>
2026-08-03 09:47:25 +0530

    Add files via upload

---
+++ natural15.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+int n;
+cout<<"enter the value of n: ";
+cin>> n;
+int i=1;
+cout<<"the first"<<n<<"natural numbers are: "<<endl;
+while(i<=n){
+    cout<<i<<" ";
+    i++;
+}
+cout<<endl;
+return 0;
+}
