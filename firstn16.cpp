080db9d1af588b64168647cdee4b6b2af7dbb159
maheshwarighugare25-source <maheshwarighugare25@gmail.com>
2026-08-03 09:44:19 +0530

    Add files via upload

---
+++ firstn16.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+int n;
+cout<<"enter the number: ";
+cin>> n;
+int sum=n*(n+1)/2;
+cout<<"sum of first" <<n<<"natural numbers= "<<sum<<endl;
+return 0;
+}
