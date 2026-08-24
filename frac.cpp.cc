1d97cbf14b717a152f1559b3b5c40cf46c621af2
maheshwarighugare25-source <maheshwarighugare25@gmail.com>
2026-08-24 10:33:33 +0530

    Add files via upload

---
+++ frac.cpp.cc
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+class fraction
+{
+   int numerator,denominator;
+public:
+   void getData()
+   {
+     cout<<"enter numerator: ";
+     cin>>numerator;
+     
+     cout<<"enter denominator: ";
+     cin>>"denominator;
+   }
+   void add(fraction f)
+   {
+     int n=numerator*
+f.denominator+
